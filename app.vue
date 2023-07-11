3a5b67bd4ca3fe550cca7ebc07fdc2c7eba5dd2a
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2023-07-11 17:06:52 +0330

    Add "Tailwind" : Start Styling Html

--- app.vue
+++ app.vue
@@ -1,22 +1,23 @@
 <template>
-  <p>{{ data }}</p>
+  <h1 class="text-3xl">This is the data : </h1>
+  <p class="text-red-600">{{ data }}</p>
 </template>
 
 <script lang="ts" setup>
 const query = gql`
   query getPosts {
-  posts{
-    id
-		title
-    body
+    posts {
+      id
+      title
+      body
+    }
+    users {
+      id
+      name
+      avatar
+      email
+    }
   }
-  users{
-    id
-		name
-    avatar
-    email
-  }
-}
-`
-const { data } = await useAsyncQuery(query)
+`;
+const { data } = await useAsyncQuery(query);
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/apollo'],
+  modules: ['@nuxtjs/apollo', '@nuxtjs/tailwindcss'],
   apollo: {
     clients: {
       default: {
