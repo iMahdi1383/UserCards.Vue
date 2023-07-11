248c5da225cf33e42f04452ac2d6f0b2d588266b
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2023-07-11 16:56:06 +0330

    "Apollo" : get data from graphql api

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <p>{{ data }}</p>
 </template>
+
+<script lang="ts" setup>
+const query = gql`
+  query getPosts {
+  posts{
+    id
+		title
+    body
+  }
+  users{
+    id
+		name
+    avatar
+    email
+  }
+}
+`
+const { data } = await useAsyncQuery(query)
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/apollo'],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'https://jsonplaceholder.ir/graphql'
+      }
+    },
+  },
+  devtools: { enabled: true },
 })
