18b3136690612316835f85754315b75ff008e1b6
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2023-07-11 17:17:26 +0330

    Add "Nuxt Image"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/apollo', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/apollo', '@nuxtjs/tailwindcss', '@nuxt/image'],
+
+  // Get data from Api
   apollo: {
     clients: {
       default: {
-        httpEndpoint: 'https://jsonplaceholder.ir/graphql'
-      }
+        httpEndpoint: 'https://jsonplaceholder.ir/graphql',
+      },
     },
   },
+
+  // Nuxt Image
+  image: {
+    domains: ['jsonplaceholder.ir'],
+  },
+
+  // Nuxt Devtools
   devtools: { enabled: true },
-})
+});
